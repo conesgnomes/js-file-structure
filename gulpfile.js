5f7e7c479b101e3add5d81769e6bbe8b9e9f87db
Connor Larsen and Karina Roush <connor@>
2017-06-26 11:30:20 -0700

    add remaining tasks and add corresponding comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,110 @@
+// make dependencies listed in package.json available to interact with //
+var utilities = require('gulp-util');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
 
-gulp.task('jsBrowserify', function() {
-  return browserify({entries: ['./js/pingpong-interface.js'] })
+// define task concatInterface. retrieve all files named -interface from the js folder and concat them together //
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+// make files ready for the browser, run concatInterface first, then place files in build/js folder //
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+// compress scripts with minify, first run jsBrowserify, then minify scripts and put them in the build folder //
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+// get all the js files that bower downloaded, concat, uglify, then place them into the build/js folder where the HTML can point to them //
+gulp.task('jsBower', function(){
+  return gulp.src(lib.ext('js').files)
+  .pipe(concat('vendor.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/js'));
+});
+
+// get all the files that end in css concat, uglify, then place them into the build/css folder where the HTML can point to them //
+gulp.task('cssBower', function(){
+  return gulp.src(lib.ext('css').files)
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('./build/css'));
+});
+
+// combines tasks jsBower and cssBower into one task to be called on later //
+gulp.task('bower', ['jsBower', 'cssBower']);
+
+// clean up any temporary files //
+gulp.task("clean", function(){
+  return del (['build', 'tmp']);
+});
+
+// tells gulp to run minify when deploying our work to a production server. If not, run jsBrowserify //
+gulp.task("build", ['clean'], function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+  gulp.start('cssBuild');
+});
+
+// runs a built in debugger of sorts on all files in the js folder //
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+// creates a server that is located in the base directory and not nested in any folders //
+gulp.task('serve', ['build'], function(){
+  browserSync.init ({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+//watch for changes in our directories and runs things automatically //
+gulp.watch(['js/*.js'], ['jsBuild']);
+gulp.watch(['bower.json'], ['bowerBuild']);
+gulp.watch(['*.html'], ['htmlBuild']);
+gulp.watch(['scss/*.scss'], ['cssBuild']);
+});
+
+// when watching for changes, constantly updates and reloads files with browswerSync.reload() //
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browswerSync.reload();
+});
+gulp.task('bowerBuild', ['bower'], function(){
+  browswerSync.reload();
+});
+gulp.task('htmlBuild', function(){
+  browswerSync.reload();
+});
+
+// takes any SASS files and compiles them down to CSS to be used //
+gulp.task('cssBuild', function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browswerSync.stream());
 });
