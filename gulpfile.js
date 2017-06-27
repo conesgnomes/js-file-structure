2f69789a0fddeaf8c8bccdb8ee6767cd9d8944e1
conesgnomes <connorlarsen@gmail.com>
2017-06-26 22:13:49 -0700

    refactor using gulp file structure and include weather app

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,25 @@ var del = require('del');
 var jshint = require('gulp-jshint');
 var buildProduction = utilities.env.production;
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+// var sass = require('gulp-sass');
+// var sourcemaps = require('gulp-sourcemaps');
+
+// tells bower-files where to find the bootstrap files we want
+
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 // define task concatInterface. retrieve all files named -interface from the js folder and concat them together //
+
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
@@ -20,6 +35,7 @@ gulp.task('concatInterface', function() {
 });
 
 // make files ready for the browser, run concatInterface first, then place files in build/js folder //
+
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({entries: ['./tmp/allConcat.js'] })
     .bundle()
@@ -28,6 +44,7 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
 });
 
 // compress scripts with minify, first run jsBrowserify, then minify scripts and put them in the build folder //
+
 gulp.task("minifyScripts", ["jsBrowserify"], function() {
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
@@ -35,29 +52,34 @@ gulp.task("minifyScripts", ["jsBrowserify"], function() {
 });
 
 // get all the js files that bower downloaded, concat, uglify, then place them into the build/js folder where the HTML can point to them //
-gulp.task('jsBower', function(){
+
+gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
-  .pipe(concat('vendor.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./build/js'));
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
 
 // get all the files that end in css concat, uglify, then place them into the build/css folder where the HTML can point to them //
-gulp.task('cssBower', function(){
+
+gulp.task('bowerCSS', function(){
   return gulp.src(lib.ext('css').files)
   .pipe(concat('vendor.css'))
   .pipe(gulp.dest('./build/css'));
 });
 
 // combines tasks jsBower and cssBower into one task to be called on later //
-gulp.task('bower', ['jsBower', 'cssBower']);
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
 
 // clean up any temporary files //
+
 gulp.task("clean", function(){
   return del (['build', 'tmp']);
 });
 
 // tells gulp to run minify when deploying our work to a production server. If not, run jsBrowserify //
+
 gulp.task("build", ['clean'], function() {
   if (buildProduction) {
     gulp.start('minifyScripts');
@@ -65,10 +87,11 @@ gulp.task("build", ['clean'], function() {
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
-  gulp.start('cssBuild');
+  // gulp.start('cssBuild');
 });
 
 // runs a built in debugger of sorts on all files in the js folder //
+
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
   .pipe(jshint())
@@ -76,6 +99,7 @@ gulp.task('jshint', function(){
 });
 
 // creates a server that is located in the base directory and not nested in any folders //
+
 gulp.task('serve', ['build'], function(){
   browserSync.init ({
     server: {
@@ -85,29 +109,32 @@ gulp.task('serve', ['build'], function(){
   });
 
 //watch for changes in our directories and runs things automatically //
+
 gulp.watch(['js/*.js'], ['jsBuild']);
 gulp.watch(['bower.json'], ['bowerBuild']);
 gulp.watch(['*.html'], ['htmlBuild']);
-gulp.watch(['scss/*.scss'], ['cssBuild']);
+// gulp.watch(['scss/*.scss'], ['cssBuild']);
 });
 
-// when watching for changes, constantly updates and reloads files with browswerSync.reload() //
+// when watching for changes, constantly updates and reloads files with browserSync.reload() //
+
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
-  browswerSync.reload();
+  browserSync.reload();
 });
 gulp.task('bowerBuild', ['bower'], function(){
-  browswerSync.reload();
+  browserSync.reload();
 });
 gulp.task('htmlBuild', function(){
-  browswerSync.reload();
+  browserSync.reload();
 });
 
 // takes any SASS files and compiles them down to CSS to be used //
+//
 gulp.task('cssBuild', function(){
   return gulp.src('scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css'))
-    .pipe(browswerSync.stream());
+    .pipe(browserSync.stream());
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -20,3 +20,22 @@ $(document).ready(function(){
     $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
   });
 });
+
+$(function() {
+  $('#time').text(moment());
+});
+
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
+
+$(function() {
+  var currentWeatherObject = new Weather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayHumidity);
+    });
+  });
